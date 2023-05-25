42a7a75b7223c3a0e469a76a03e284c9c27045f1
ValentynKrasovskyi <krasovskyi00@gmail.com>
2023-05-25 12:31:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,6 +18,17 @@ export class App extends Component {
       filter: '',
     };
   }
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts: contacts });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addContact = ({ name, number }) => {
     const added = this.state.contacts.some(
       contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
